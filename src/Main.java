f571bed8c0ffaa167846a260abdf58d7efa7bf99
hostgay <bektur.tilenov@mail.ru>
2024-01-17 17:08:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    int b;
+    public static void test( int number){
+        System.out.println();
+
+
+        int[] numbers = {1, 23 , 4, 54, 54};
+
+        System.out.println(numbers[2] + numbers[1]);
+
+
+
+        int[][] num2D = new int[3][4];
+
+        System.out.println(Arrays.toString(num2D));
+
+        String[][] students = {{"Jack", "joe"}, {}};
+
+    }
+
+}
